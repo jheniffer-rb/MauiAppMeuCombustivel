691e8304b75705ff133745bf8fe50072f32de598
JHENIFFER RODRIGUES BENEVIDES <jheniffer.benevides@fatec.sp.gov.br>
2024-12-04 22:51:38 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuCombustivel/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppMeuCombustivel //Define o namespace do projeto
+{
+    public partial class App : Application // Define a classe App que herda de Application.
+    {
+        public App() // Define o construtor da classe App.
+        {
+            InitializeComponent(); // Inicializa os componentes da aplicação.
+
+            MainPage = new AppShell();// Define a página principal da aplicação como a AppShell.
+        }
+    } // Fecha a classe App
+} // Fecha o namespace
---
+++ MauiAppMeuCombustivel/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿namespace MauiAppMeuCombustivel 
+{
+    public partial class MainPage : ContentPage 
+    {
+        public MainPage() 
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            try 
+            {
+                // Converte o texto dos campos de texto para double.
+                double etanol = Convert.ToDouble(txt_etanol.Text); 
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                // Verifica se o etanol está compensando.
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "O Etanol está compensando."; 
+                else
+                {
+                    msg = "A Gasolina está compensando."; 
+                }
+
+                DisplayAlert("Calculado", msg, "OK");
+            } 
+            catch (Exception ex) 
+            {
+                
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+        } 
+    } 
+} 
